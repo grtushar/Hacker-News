3578b4fc191c1f10ab1f18be552c59ba674fe925
grtushar <md.tushar_91@yahoo.com>
2020-05-14 05:13:33 +0600

    :zap: update fetch article response from future to stream

--- lib/repository/ArticleRepository.dart
+++ lib/repository/ArticleRepository.dart
@@ -1,17 +1,20 @@
 import 'package:hackernews/model/Article.dart';
 import 'package:hackernews/network/HackerNewsApi.dart';
 
-Future<List<Article>> getArticles() async {
-	final articles = List<Article>();
-	fetchArticles().then((articleIds) {
-//		for(int articleId in articleIds) {
-		for(int i = 0; i < 50; i++) {
-			int articleId = articleIds[i];
-			fetchArticle(articleId).then((article) {
-				articles.add(article);
-			});
-		}
-	});
-	
-	return articles;
+Stream<Article> getArticles() async* {
+//	final articles = List<Article>();
+//	fetchArticles().then((articleIds) {
+////		for(int articleId in articleIds) {
+//		for(int i = 0; i < 50; i++) {
+//			int articleId = articleIds[i];
+//			fetchArticle(articleId).then((article) {
+//				yield article;
+////				articles.add(article);
+//			});
+//		}
+//	});
+	final ids = await fetchArticles();
+	for(int i = 0; i < 15; i++) {
+		yield await fetchArticle(ids[i]);
+	}
 }
