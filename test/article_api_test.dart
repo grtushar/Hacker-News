40e0a42e893e2fb95d7082980e8c68cf364ca5c9
grtushar <md.tushar_91@yahoo.com>
2020-05-14 18:11:28 +0600

    :zap: update api test condition

--- test/article_api_test.dart
+++ test/article_api_test.dart
@@ -3,16 +3,13 @@ import 'package:test/test.dart';
 
 void main() {
 	test("Fetching article ids", () async {
-		const articleIdMatcher = 23155647;
-		const articleAuthorMatcher = "minimaxir";
-		
 		int firstArticleId = (await fetchArticles()).first;
 //		await fetchArticles().then((values) {
 //			firstArticleId = values.first;
 //		});
-		expect(firstArticleId, articleIdMatcher);
+		expect(firstArticleId, isNotNull);
 		
 		String firstArticleAuthor = (await fetchArticle(firstArticleId)).by;
-		expect(firstArticleAuthor, articleAuthorMatcher);
+		expect(firstArticleAuthor, isNotNull);
 	});
 }
